dbaaf389b8ea6710c8675fabba26b86f471967e4
Gustavo <pow10@txstate.edu>
2026-07-12 19:15:42 -0500

    basic scoreboard html sructure + css style complete

---
+++ index.js
@@ -0,0 +1,22 @@
+homeNum = 0
+guestNum = 0
+
+function homeBtn1() {
+    
+}
+function homeBtn2() {
+
+}
+function homeBtn3() {
+
+}
+
+function guestBtn1() {
+
+}
+function guestBtn2() {
+
+}
+function guestBtn3() {
+
+}
