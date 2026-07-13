d652282277f404a90f501718f274dc5b87d45d47
Gustavo <pow10@txstate.edu>
2026-07-12 19:46:40 -0500

    added highlighting element to the higher score team

--- index.js
+++ index.js
@@ -14,30 +14,47 @@ function updateScore(element, value) {
     } else {
         element.style.fontSize = "4.5rem"
     }
+
+}
+
+function highlightScore(element1, element2) {
+    if (homeNum > guestNum) {
+        element1.style.border = "2px solid yellow"
+        element2.style.border = "none"
+    } else {
+        element2.style.border = "2px solid yellow"
+        element1.style.border = "none"    
+    }
 }
 
 function homeBtn1() {
     homeNum += 1
     updateScore(homeEl, homeNum)
+    highlightScore(homeEl, guestEl)
 }
 function homeBtn2() {
     homeNum += 2
     updateScore(homeEl, homeNum)
+    highlightScore(homeEl, guestEl)
 }
 function homeBtn3() {
     homeNum += 3
     updateScore(homeEl, homeNum)
+    highlightScore(homeEl, guestEl)
 }
 
 function guestBtn1() {
     guestNum += 1
     updateScore(guestEl, guestNum)
+    highlightScore(homeEl, guestEl)
 }
 function guestBtn2() {
     guestNum += 2
     updateScore(guestEl, guestNum)
+    highlightScore(homeEl, guestEl)
 }
 function guestBtn3() {
     guestNum += 3
     updateScore(guestEl, guestNum)
+    highlightScore(homeEl, guestEl)
 }
