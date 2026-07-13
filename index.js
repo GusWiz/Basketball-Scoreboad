418c49c321610aa38c4784a768bb4efe4a1126a3
Gustavo <pow10@txstate.edu>
2026-07-12 19:26:35 -0500

    Added function for onclick and confirmed functionality.

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 homeNum = 0
 guestNum = 0
 
+homeEl = document.getElementById("home")
+guestEl = document.getElementById("guest")
+
 function homeBtn1() {
-    
+    homeNum += 1
+    homeEl.textContent = homeNum
 }
 function homeBtn2() {
-
+    homeNum += 2
+    homeEl.textContent = homeNum
 }
 function homeBtn3() {
-
+    homeNum += 3
+    homeEl.textContent = homeNum
 }
 
 function guestBtn1() {
-
+    guestNum += 1
+    guestEl.textContent = guestNum
 }
 function guestBtn2() {
-
+    guestNum += 2
+    guestEl.textContent = guestNum
 }
 function guestBtn3() {
-
+    guestNum += 3
+    guestEl.textContent = guestNum
 }
